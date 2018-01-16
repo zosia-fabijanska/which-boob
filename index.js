9fe674b03e25840f9a7fe4808cc369a5b9b98710
Zosia Fabijanska <zosia.fabijanska@gmail.com>
2018-01-16 22:42:21 +1100

    add color scroll functionality

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-function setup_colors() {
-    var raw_colors = [
-        '#6fb1bc',
-        '#C3ADA5',
-        
-    ];
-    if ($(window).width() <= 480) {
-        raw_colors = ['#C7F8FE'];
-    }
-
-    var colors = [];
-    for (var i=0; i<raw_colors.length; i++) {
-        colors.push({
-            color: raw_colors[i],
-            position: $(window).height() * i
-        });
-    };
-
-    $('body').colorScroll({
-        colors: colors
+$(function() {
+    //store element
+    const zosiaElement = $(".zosia-section");
+    $(window).scroll(function() {
+        if ($(this).scrollTop() <= 400) {
+            zosiaElement.css("background", "#6fb1bc");
+        } else if ($(this).scrollTop() <= 900) {
+            zosiaElement.css("background", "#e67e22");
+        } else if ($(this).scrollTop() <= 1400) {
+            zosiaElement.css("background", "#8e44ad");
+        } else if ($(this).scrollTop() <= 1900) {
+            zosiaElement.css("background", "#3498db");
+        } else if ($(this).scrollTop() <= 2500) {
+            zosiaElement.css("background", "#2ecc71");
+        }
     });
-}
-
-$(document).ready(function(){
-  setup_colors();
 });
-
-$(window).resize(setup_colors);
