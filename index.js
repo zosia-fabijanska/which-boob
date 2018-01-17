a8230678a9687e40e8c5dc46e1e72dadfe167aae
Zosia Fabijanska <zosia.fabijanska@gmail.com>
2018-01-17 11:00:47 +1100

    added structure and color scroll panels

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ $(function() {
         if ($(this).scrollTop() <= 400) {
             zosiaElement.css("background", "#6fb1bc");
         } else if ($(this).scrollTop() <= 900) {
-            zosiaElement.css("background", "#e67e22");
+            zosiaElement.css("background", "#94f7d3");
         } else if ($(this).scrollTop() <= 1400) {
-            zosiaElement.css("background", "#8e44ad");
+            zosiaElement.css("background", "#fcdc8a");
         } else if ($(this).scrollTop() <= 1900) {
-            zosiaElement.css("background", "#3498db");
+            zosiaElement.css("background", "#f4a4a4");
         } else if ($(this).scrollTop() <= 2500) {
-            zosiaElement.css("background", "#2ecc71");
+            zosiaElement.css("background", "#dd98f2");
         }
     });
 });
