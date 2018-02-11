3db4dfb8ff87b86be1b05c6e3639cefcf025a78b
Zosia Fabijanska <zosia.fabijanska@gmail.com>
2018-02-12 09:01:59 +1100

    scroll functionality removed

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-$(() => {
-    //store element
-    const zosiaElement = $(".zosia-section");
-    $(window).scroll(() => {
-        if ($(this).scrollTop() <= 400) {
-            zosiaElement.css("background", "#6fb1bc");
-        } else if ($(this).scrollTop() <= 1200) {
-            zosiaElement.css("background", "#94f7d3");
-        } else if ($(this).scrollTop() <= 2000) {
-            zosiaElement.css("background", "#fcdc8a");
-        } else if ($(this).scrollTop() <= 2800) {
-            zosiaElement.css("background", "#f4a4a4");
-        } else if ($(this).scrollTop() <= 3600) {
-            zosiaElement.css("background", "#dd98f2");
-        }
-    });
+// $(() => {
+//     //store element
+//     const zosiaElement = $(".zosia-section");
+//     $(window).scroll(() => {
+//         if ($(this).scrollTop() <= 400) {
+//             zosiaElement.css("background", "#6fb1bc");
+//         } else if ($(this).scrollTop() <= 1200) {
+//             zosiaElement.css("background", "#94f7d3");
+//         } else if ($(this).scrollTop() <= 2000) {
+//             zosiaElement.css("background", "#fcdc8a");
+//         } else if ($(this).scrollTop() <= 2800) {
+//             zosiaElement.css("background", "#f4a4a4");
+//         } else if ($(this).scrollTop() <= 3600) {
+//             zosiaElement.css("background", "#dd98f2");
+//         }
+//     });
 
-    const elementHeight = $('#built').offset().top;
-    $('.scroll-logo').on('click', () => {
-        window.scroll({
-            top: elementHeight, 
-            left: 0, 
-            behavior: 'smooth' 
-        });
-    }); 
-});
+//     const elementHeight = $('#built').offset().top;
+//     $('.scroll-logo').on('click', () => {
+//         window.scroll({
+//             top: elementHeight, 
+//             left: 0, 
+//             behavior: 'smooth' 
+//         });
+//     }); 
+// });
