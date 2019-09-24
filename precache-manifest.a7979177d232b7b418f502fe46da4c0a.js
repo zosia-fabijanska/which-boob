242431a9d90636fb0cf05eb459ee29d59da17b3b
Zosia Fabijanska <zosia.fabijanska@gmail.com>
2019-09-24 22:28:39 +1000

    Updates

---
+++ precache-manifest.a7979177d232b7b418f502fe46da4c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78357475dea46c50fa5461835505322",
+    "url": "/which-boob/index.html"
+  },
+  {
+    "revision": "eecff5b247f95a8f81bc",
+    "url": "/which-boob/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3d660908113fcfb19233",
+    "url": "/which-boob/static/js/2.32564228.chunk.js"
+  },
+  {
+    "revision": "eecff5b247f95a8f81bc",
+    "url": "/which-boob/static/js/main.09670154.chunk.js"
+  },
+  {
+    "revision": "6a93de54571938658f7b",
+    "url": "/which-boob/static/js/runtime-main.4e78cb0e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/which-boob/static/media/logo.25bf045c.svg"
+  }
+]);
